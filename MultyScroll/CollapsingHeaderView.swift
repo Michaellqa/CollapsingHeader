8bd3599eb994ba3e0fbaa2cae51757ce63773f59
Michail Krasyukov <amnezea@gmail.com>
2018-07-04 21:29:27 +0300

    Initial commit. Struggling making constraints match up

---
+++ MultyScroll/CollapsingHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  CollapsingHeaderView.swift
+//  MultyScroll
+//
+//  Created by Micky on 04/07/2018.
+//  Copyright © 2018 Micky. All rights reserved.
+//
+
+import UIKit
+
+class CollapsingHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
