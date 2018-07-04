444f7c32afed10dc8e3648ef59903cf17aa584cc
Michail Krasyukov <amnezea@gmail.com>
2018-07-04 21:30:58 +0300

    GIT ADD PERIOD. NEVER FORGET

--- MultyScroll/CollapsingHeaderView.swift
+++ MultyScroll/CollapsingHeaderView.swift
@@ -9,13 +9,85 @@
 import UIKit
 
 class CollapsingHeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var minVisibleHeight: CGFloat = 0.0
+    var onHeightChanged: ((_ height: CGFloat, _ percentDelta: CGFloat) -> ())? = nil
+    
+    private var currentHeight: CGFloat = 0.0
+    private var viewHeight: CGFloat = 0.0
+    private let internalView: UIView
+    
+    private lazy var panGestureRecognizer: UIPanGestureRecognizer = {
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture))
+        return recognizer
+    }()
+    
+    init(minVisibleHeight: CGFloat, headerView: UIView) {
+        self.internalView = headerView
+        super.init(frame: headerView.frame)
+        
+        self.minVisibleHeight = minVisibleHeight
+        currentHeight = frame.height
+        viewHeight = frame.height
+        
+        addSubview(internalView)
+        internalView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            internalView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+//            internalView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+//            internalView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+////            internalView.topAnchor.constraint(equalTo: self.topAnchor)
+//            internalView.heightAnchor.constraint(equalToConstant: 200),
+//            internalView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        internalView.addGestureRecognizer(panGestureRecognizer)
     }
-    */
 
+    
+    @objc func handlePanGesture() {
+        switch panGestureRecognizer.state {
+        case .began: print("began")
+        case .changed:
+            let transition = panGestureRecognizer.translation(in: subviews.first!)
+            changeCurrentHeight(delta: transition.y)
+        case .ended: print("ended")
+        default: break
+        }
+        // prevent cummulative effect
+        panGestureRecognizer.setTranslation(.zero, in: self)
+    }
+    
+    private func changeCurrentHeight(delta: CGFloat) {
+        guard (delta > 0 && currentHeight < viewHeight) || (delta < 0 && currentHeight > minVisibleHeight) else {
+            return
+        }
+        let newHeight = internalView.frame.size.height + delta
+//        frame.size.height = newHeight
+        internalView.frame.size.height = newHeight
+        
+        print("whole \(frame)")
+        print("content \(internalView.frame)")
+        currentHeight = newHeight
+        
+        onHeightChanged?(newHeight, 0)
+    }
+    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        commonInit()
+//    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        internalView = UIView()
+        super.init(coder: aDecoder)
+    }
+    
+//    private func commonInit() {
+//        Bundle.main.loadNibNamed("CollapsingHeaderView", owner: self, options: nil)
+//        addSubview(contentView)
+//        contentView.frame = self.bounds
+//        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//    }
+    
 }
--- MultyScroll/ViewController.swift
+++ MultyScroll/ViewController.swift
@@ -8,18 +8,86 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .blue
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    lazy var internalHeaderView: UIView = {
+        let width = UIScreen.main.bounds.width
+        let internalView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 200))
+        internalView.translatesAutoresizingMaskIntoConstraints = false
+        internalView.backgroundColor = UIColor.magenta
+        
+        let v = UIView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+        v.layer.cornerRadius = 25
+        v.backgroundColor = .green
+        internalView.addSubview(v)
+        v.center = internalView.center
+        
+        return internalView
+    }()
+    lazy var headerView: CollapsingHeaderView = {
+        let header = CollapsingHeaderView(minVisibleHeight: 40, headerView: internalHeaderView)
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.onHeightChanged = { _, _ in
+//            self.tableView.setNeedsLayout()
+//            self.tableView.layoutIfNeeded()
+            self.tableView.reloadData()
+        }
+        return header
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        setupTable()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupTable() {
+        view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        tableView.tableHeaderView = headerView
     }
-
-
+    
 }
+
+extension ViewController: UITableViewDataSource {
+    
+    var rus: [UIColor] { return [.red, .yellow, .black] }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = rus[indexPath.row % 3]
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
 
